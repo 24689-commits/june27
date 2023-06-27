00fe36bd1647462ef31500445fe14bc6c57e1e31
Aphelele2 <aphelelejoyi@gmail.com>
2023-06-27 16:33:18 +0200

    june27

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import Products from '@/components/Products.vue'
+    export default {
+        components:{
+            Products
+        }
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
